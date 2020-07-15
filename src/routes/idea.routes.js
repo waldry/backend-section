ba6ab744439358820d451d091056a5caa71fdbb6
Waldry <waldrymaster@outlook.com>
2020-07-15 18:37:10 -0400

    [ADD] Swagger doc for apis

--- src/routes/idea.routes.js
+++ src/routes/idea.routes.js
@@ -1,16 +1,16 @@
 const { Router } = require("express");
-const { ParseIntMiddleware } = require("../middlewares");
+const { ParseIntMiddleware, AuthMiddleware } = require("../middlewares");
 
 module.exports = function ({ IdeaController }) {
   const router = Router();
 
-  router.get("", ParseIntMiddleware, IdeaController.getAll);
+  router.get("", [ParseIntMiddleware], IdeaController.getAll);
   router.get("/:ideaId", IdeaController.get);
   router.get("/:ideaId/all", IdeaController.getUserIdeas);
   router.post("", IdeaController.create);
-  router.patch("/:ideaId", IdeaController.update);
-  router.delete("/:ideaId", IdeaController.delete);
-  router.post(":ideaId/upvotes", IdeaController.upvoteIdea);
-  router.post(":ideaId/downvotes", IdeaController.downvoteIdea);
+  router.patch("/:ideaId", AuthMiddleware, IdeaController.update);
+  router.delete("/:ideaId", AuthMiddleware, IdeaController.delete);
+  router.post(":ideaId/upvotes", AuthMiddleware, IdeaController.upvoteIdea);
+  router.post(":ideaId/downvotes", AuthMiddleware, IdeaController.downvoteIdea);
   return router;
 };
